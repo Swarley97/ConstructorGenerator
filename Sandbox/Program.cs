e54196c6ff1b2123aca2cf8c8378906d44569027
Tim N <nootny97@outlook.de>
2022-02-21 23:08:00 +0100

    optional parameter and constructor accessibility

---
+++ Sandbox/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using ConstructorGenerator;
+namespace Sandbox;
+
+internal abstract partial class TestClass<T1, T2, T3, T4>
+{
+    [ConstructorDependency]
+    private readonly Semaphore _sem;
+
+
+    
+}
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
